50e1fb825b2b4ed4b7ea50801ee5e092aa0b791b
Phi <phi@kernel-tools.com>
2023-10-02 05:43:05 +0200

    Remome unneeded 'expr' fork/exec in order to ease the debug sessions

--- src/cmd/ksh93/tests/subrace.sh
+++ src/cmd/ksh93/tests/subrace.sh
@@ -26,6 +26,6 @@ for((;;))
 { $(expr A : B >/dev/null) && ((e=1))
   ((SECONDS>2)) && break;
 }
-((!$#&&e)) && err_exit '$(expr A : B >/dev/null)'
+((!$#&&e)) && err_exit '$(:>/dev/null)'
 # ======
 exit $((Errors<125?Errors:125))
